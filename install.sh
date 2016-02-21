7537b19170825ef4f9921eb7c4829a1b9e6393a9
Kordan Ou <kordan@hitripod.com>
2016-02-21 15:02:53 +0800

    organize the directory structure, rename '.vim' to '.dotfiles'

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VIMHOME=~/.vim
+DOTFILES_HOME=~/.dotfiles
 
 warn() {
     echo "$1" >&2
@@ -10,25 +10,41 @@ die() {
     exit 1
 }
 
-[ -e "$VIMHOME/vimrc" ]                 && die "Kordan's dotfiles is already installed."
+[ -e "$DOTFILES_HOME/vim/vimrc" ]       && die "Kordan's dotfiles is already installed."
 [ -f "~/.vim" ] && [ ! -L "~/.vim" ]    && die "~/.vim already exists."
 [ -f "~/.vimrc"] && [ ! -L "~/.vimrc" ] && die "~/.vimrc already exists."
 
 # Check out the prerequisites
-git clone https://github.com/hitripod/dotfiles.git ~/.vim
+git clone https://github.com/hitripod/dotfiles.git ~/.dotfiles
+ln -sf $DOTFILES_HOME/vim ~/.vim  
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone git://github.com/tarruda/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone git://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-# Make the symbolic links
-ln -sf ~/.vim/vimrc ~/.vimrc ||
-ln -sf ~/.vim/gvimrc ~/.gvimrc
-rm -rf ~/.zshrc && ln -s ~/.vim/zsh/oh-my-zsh.zshrc ~/.zshrc 
-ln -sf ~/.vim/zsh/zshrc ~/.oh-my-zsh/custom/kordan.zsh
-mkdir -p ~/.oh-my-zsh/custom/lib && ln -sf ~/.vim/zsh/key-bindings.zsh ~/.oh-my-zsh/custom/lib/key-bindings.zsh
-ln -sf ~/.vim/git/gitconfig ~/.gitconfig
+# Make the symbolic links - VIM
+ln -sf ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/.dotfiles/vim/gvimrc ~/.gvimrc
+# Make the symbolic links - ZSH
+rm -rf ~/.zshrc && ln -s ~/.dotfiles/zsh/oh-my-zsh.zshrc ~/.zshrc 
+ln -sf ~/.dotfiles/zsh/zshrc ~/.oh-my-zsh/custom/kordan.zsh
+mkdir -p ~/.oh-my-zsh/custom/lib && \
+ln -sf ~/.dotfiles/zsh/key-bindings.zsh ~/.oh-my-zsh/custom/lib/key-bindings.zsh
+# Make the symbolic links - GIT
+ln -sf ~/.dotfiles/git/gitconfig ~/.gitconfig
+# Make the symbolic links - TMUX
+ln -sf ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 # Initilize and update those plugins using Vundle
 vim +PluginInstall +qall
 echo "Kordan's dotfile is installed."
+
+clean() {
+    rm -rf ~/.vimrc
+    rm -rf ~/.gvimrc
+    rm -rf ~/.zshrc 
+    rm -rf ~/.oh-my-zsh/custom/kordan.zsh
+    rm -rf ~/.oh-my-zsh/custom/lib/key-bindings.zsh
+    rm -rf ~/.gitconfig
+    rm -rf ~/.tmux.conf
+}
